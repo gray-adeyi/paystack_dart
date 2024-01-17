a76713a219086c1977ecd8a129f881f6c0775cb6
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-17 09:40:36 +0100

    Add exceptions

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,18 @@
+class BaseException implements Exception {
+  final String message;
+
+  BaseException(this.message);
+
+  @override
+  String toString() {
+    return '$runtimeType: $message';
+  }
+}
+
+class MissingSecretKeyException extends BaseException {
+  MissingSecretKeyException(super.message);
+}
+
+class UnsupportedHttpMethodException extends BaseException {
+  UnsupportedHttpMethodException(super.message);
+}

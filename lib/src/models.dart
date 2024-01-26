acfbcd8322f1c6c1cb74b100d4f299afb071eed9
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-26 11:18:59 +0100

    Add project models

---
+++ lib/src/models.dart
@@ -0,0 +1,135 @@
+import './enums.dart';
+
+class TerminalData {
+  final String id;
+  final String reference;
+
+  TerminalData(this.id, this.reference);
+
+  Map<String, String> toMap() {
+    return {
+      "id": id,
+      "reference": reference,
+    };
+  }
+}
+
+class Subaccount {
+  final String id;
+  final double share;
+
+  Subaccount(this.id, this.share);
+
+  Map<String, String> toMap() {
+    return {
+      "subaccount": id,
+      "share": share.toString(),
+    };
+  }
+
+// TODO: implement a factory that generates a List<Subaccount> from
+// a list of Map<String, String>
+}
+
+class LineItem {
+  final String name;
+  final int amount;
+  final int quantity;
+
+  LineItem(this.name, this.amount, this.quantity);
+
+  Map<String, dynamic> toMap() {
+    return {'name': name, "amount": amount, 'quantity': quantity};
+  }
+}
+
+class Tax {
+  final String name;
+  final int amount;
+
+  Tax(this.name, this.amount);
+
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'amount': amount};
+  }
+}
+
+class TransferRecipient {
+  final FinancialChannel type;
+  final String name;
+  String? accountNumber;
+  String? bankCode;
+  String? description;
+  Currency? currency;
+  String? authorizationCode;
+  Map<String, dynamic>? metadata;
+
+  TransferRecipient(this.type, this.name,
+      {this.accountNumber,
+      this.bankCode,
+      this.description,
+      this.currency,
+      this.authorizationCode,
+      this.metadata}) {
+    if (accountNumber == null && authorizationCode == null) {
+      throw ArgumentError("accountNumber and authorizationCode can't both "
+          "be missing, please provide only one of this two missing parameters");
+    }
+    if (accountNumber != null && authorizationCode != null) {
+      throw ArgumentError(
+          "you can't provide both the accountNumber and authorizationCode. "
+          "please provide either of the two parameters");
+    }
+    if (accountNumber != null && bankCode == null) {
+      throw ArgumentError(
+          'bankCode is required when accountNumber is provided');
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'type': type.paystackValue,
+      'name': name,
+      'account_number': accountNumber,
+      'bank_code': bankCode,
+      'description': description,
+      'currency': currency?.paystackValue,
+      'authorization_code': authorizationCode,
+      'metadata': metadata
+    };
+  }
+}
+
+class Transfer {
+  final int amount;
+  final String recipient;
+  String? reference;
+  String? reason;
+
+  Transfer(this.amount, this.recipient, this.reference, this.reason);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'amount': amount,
+      'recipient': recipient,
+      'reference': reference,
+      'reason': reason
+    };
+  }
+}
+
+class Charge {
+  final String authorization;
+  final int amount;
+  final String reference;
+
+  Charge(this.authorization, this.amount, this.reference);
+
+  Map<String, dynamic> toMap() {
+    return {
+      "authorization": authorization,
+      "amount": amount,
+      "reference": reference,
+    };
+  }
+}

92a93be486e1e191b834a975f18f5af9b161624c
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-26 11:09:04 +0100

    Implement TransactionSplitClient

--- lib/src/clients/transaction_split_client.dart
+++ lib/src/clients/transaction_split_client.dart
@@ -1,33 +1,17 @@
 import '../base_client.dart';
 import '../enums.dart';
+import '../models.dart';
 
-class Subaccount {
-  final String id;
-  final double share;
+class TransactionSplitClient extends BaseClient {
+  TransactionSplitClient({super.secretKey});
 
-  Subaccount(this.id, this.share);
-
-  Map<String, String> toMap() {
-    return {
-      "subaccount": id,
-      "share": share.toString(),
-    };
-  }
-
-// TODO: implement a factory that generates a List<Subaccount> from
-// a list of Map<String, String>
-}
-
-class TransactionSplitsClient extends BaseClient {
-  TransactionSplitsClient({super.secretKey});
-
-  Response createSplit(
+  Future<Response> createSplit(
       String name,
       SplitMode type,
       Currency currency,
       List<Subaccount> subaccounts,
       SubaccountChargeBearer bearerType,
-      String bearerSubaccount) {
+      String bearerSubaccount) async {
     Map<String, dynamic> data = {
       "name": name,
       "type": type.name,
@@ -36,49 +20,52 @@ class TransactionSplitsClient extends BaseClient {
       "bearer_type": bearerType,
       "bearer_subaccount": bearerSubaccount
     };
-    return call(Uri.https(baseUrl, "/split"), HttpMethod.post, data: data);
+    return await call(Uri.https(baseUrl, "/split"), HttpMethod.post,
+        data: data);
   }
 
-  Response all(String name, bool active,
+  Future<Response> all(String name, bool active,
       {String? sortBy,
       int perPage = 50,
       int page = 1,
       String? from,
-      String? to}) {
+      String? to}) async {
     var queryParameters = normalizeQueryParameters({
       'perPage': perPage,
       'page': page,
       'from': from,
       'to': to,
     });
-    return call(Uri.https(baseUrl, "/split", queryParameters), HttpMethod.get);
+    return await call(
+        Uri.https(baseUrl, "/split", queryParameters), HttpMethod.get);
   }
 
-  Response fetchOne(String id) {
-    return call(Uri.https(baseUrl, "/split/$id"), HttpMethod.get);
+  Future<Response> fetchOne(String id) async {
+    return await call(Uri.https(baseUrl, "/split/$id"), HttpMethod.get);
   }
 
-  Response update(String id, String name, bool active,
-      {SplitMode? type, String? bearerSubaccount}) {
+  Future<Response> update(String id, String name, bool active,
+      {SplitMode? type, String? bearerSubaccount}) async {
     var data = {
       'name': name,
       'active': active,
       'bearer_type': type?.name,
       'bearer_subaccount': bearerSubaccount
     };
-    return call(Uri.https(baseUrl, "/split/$id"), HttpMethod.put, data: data);
+    return await call(Uri.https(baseUrl, "/split/$id"), HttpMethod.put,
+        data: data);
   }
 
-  Response add(String id, String subaccount, double share) {
+  Future<Response> add(String id, String subaccount, double share) async {
     var data = {'subaccount': subaccount, 'share': share};
-    return call(
+    return await call(
         Uri.https(baseUrl, "/split/$id/subaccount/add"), HttpMethod.post,
         data: data);
   }
 
-  Response remove(String id, String subaccount) {
+  Future<Response> remove(String id, String subaccount) async {
     var data = {'subaccount': subaccount};
-    return call(
+    return await call(
         Uri.https(baseUrl, "/split/$id/subaccount/remove"), HttpMethod.post,
         data: data);
   }

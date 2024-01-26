0413745a7ebbbc4461499d0bd40eb358ed1a6660
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-26 11:07:39 +0100

    Implement ApplePay client

---
+++ lib/src/clients/apple_pay_client.dart
@@ -0,0 +1,24 @@
+import '../base_client.dart';
+import '../enums.dart';
+
+class ApplePayClient extends BaseClient {
+  Future<Response> register(String domainName) async {
+    return await call(Uri.https(baseUrl, '/apple-pay/domain'), HttpMethod.post,
+        data: {'domainName': domainName});
+  }
+
+  Future<Response> all(bool useCursor, {String? next, String? previous}) async {
+    var queryParameters = normalizeQueryParameters({
+      'next': next,
+      'previous': previous,
+    });
+    return await call(Uri.https(baseUrl, '/apple-pay/domain', queryParameters),
+        HttpMethod.get);
+  }
+
+  Future<Response> unregister(String domainName) async {
+    return await call(
+        Uri.https(baseUrl, '/apple-pay/domain'), HttpMethod.delete,
+        data: {'domainName': domainName});
+  }
+}

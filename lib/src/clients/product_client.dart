f4b574a94de88b3e87fa099b078199f194b046a0
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-01-26 11:14:40 +0100

    Implement ProductClient

---
+++ lib/src/clients/product_client.dart
@@ -0,0 +1,50 @@
+import '../base_client.dart';
+import '../enums.dart';
+
+class ProductClient extends BaseClient {
+  Future<Response> create(
+      String name, String description, int price, Currency currency,
+      {bool? unlimited, int? quantity}) async {
+    var data = {
+      'name': name,
+      'description': description,
+      'price': price,
+      'currency': currency.paystackValue,
+      'unlimited': unlimited,
+      'quantity': quantity,
+    };
+    return await call(Uri.https(baseUrl, '/product'), HttpMethod.post,
+        data: data);
+  }
+
+  Future<Response> all(
+      {int perPage = 50, int page = 1, String? from, String? to}) async {
+    var queryParameters = normalizeQueryParameters({
+      'perPage': perPage,
+      'page': page,
+      'from': from,
+      'to': to,
+    });
+    return await call(
+        Uri.https(baseUrl, '/product', queryParameters), HttpMethod.get);
+  }
+
+  Future<Response> fetchOne(String id) async {
+    return await call(Uri.https(baseUrl, '/product/$id'), HttpMethod.get);
+  }
+
+  Future<Response> update(
+      String id, String name, String description, int price, Currency currency,
+      {bool? unlimited, int? quantity}) async {
+    var data = {
+      'name': name,
+      'description': description,
+      'price': price,
+      'currency': currency.paystackValue,
+      'unlimited': unlimited,
+      'quantity': quantity,
+    };
+    return await call(Uri.https(baseUrl, '/product/$id'), HttpMethod.put,
+        data: data);
+  }
+}

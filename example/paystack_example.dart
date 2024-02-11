aa7fd2598ab5f74d56c25e97c5eeca3bd5ba0b2f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-02-11 09:10:48 +0100

    Add package example

--- example/paystack_example.dart
+++ example/paystack_example.dart
@@ -1,6 +1,15 @@
 import 'package:paystack/paystack.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  // The secretKey optional name parameter can be omitted if provided in the
+  // environmental variables. as 'PAYSTACK_SECRET_KEY'
+  var client = PaystackClient(secretKey: "<your paystack secret key>");
+  // Retrieve all the customers on your integrations.
+  var response = await client.customers.all();
+  print("the statusCode of the last call is ${response.statusCode}");
+  print(response.data);
+
+  // Initialize a transaction on you integration.
+  response = await client.transactions.initialize(10000, "johndoe@example.com");
+  print(response.data);
 }

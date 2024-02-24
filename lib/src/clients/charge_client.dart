c8d2db40effd4c60d22da75cf006798dcb91e2cb
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-02-24 12:36:27 +0100

    Add documentation for ChargeClient

--- lib/src/clients/charge_client.dart
+++ lib/src/clients/charge_client.dart
@@ -1,9 +1,33 @@
 import '../base_client.dart';
 import '../enums.dart';
 
+/// It provides methods that mirrors endpoints provided by
+/// Paystack's Charge API which allows you to configure payment channel
+/// of your choice when initiating a payment.
 class ChargeClient extends BaseClient {
+  /// Create an instance of [ChargeClient].
+  ///
+  /// [secretKey] is your Paystack integration key. If omitted,
+  /// [ChargeClient] tries to load it from your environmental
+  /// variables with a key stored in its [secretKeyEnvironmentalVariableName]
+  /// which has a default of "PAYSTACK_SECRET_KEY". i.e. If you provide
+  /// your Paystack integration secret key in your environmental variables
+  /// as `PAYSTACK_SECRET_KEY=<your secret key>`, you don't need to provide
+  /// a [secretKey] on instantiation of [ChargeClient].
+  /// There is no need to instantiate this class directly unless you only
+  /// need to use features specific to this client as it is available in
+  /// the all encompassing [PaystackClient] via `charges` binding
+  /// ## Example
+  /// ```dart
+  /// var client = ChargeClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.submitOtp("234567");
+  /// // The Following code is an equivalent of the code above.
+  /// var client = PaystackClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.charges.submitOtp("234567");
+  /// ```
   ChargeClient({super.secretKey});
 
+  /// Initiate a payment by integrating the payment channel of your choice.
   Future<Response> create(
     String email,
     int amount, {
@@ -52,28 +76,33 @@ class ChargeClient extends BaseClient {
         data: data);
   }
 
+  /// Submit pin to continue charge.
   Future<Response> submitPin(String pin, String reference) async {
     return await call(Uri.https(baseUrl, '/charge/submit_pin'), HttpMethod.post,
         data: {'pin': pin, 'reference': reference});
   }
 
+  /// Submit otp to complete charge.
   Future<Response> submitOtp(String otp, String reference) async {
     return await call(Uri.https(baseUrl, '/charge/submit_otp'), HttpMethod.post,
         data: {'otp': otp, 'reference': reference});
   }
 
+  /// Submit phone number when requested.
   Future<Response> submitPhone(String phone, String reference) async {
     return await call(
         Uri.https(baseUrl, '/charge/submit_phone'), HttpMethod.post,
         data: {'phone': phone, 'reference': reference});
   }
 
+  /// Submit birthday when requested.
   Future<Response> submitBirthday(String birthday, String reference) async {
     return await call(
         Uri.https(baseUrl, '/charge/submit_birthday'), HttpMethod.post,
         data: {'birthday': birthday, 'reference': reference});
   }
 
+  /// Submit address to continue charge.
   Future<Response> submitAddress(String address, String reference, String city,
       String state, String zipcode) async {
     var data = {
@@ -88,6 +117,14 @@ class ChargeClient extends BaseClient {
         data: data);
   }
 
+  /// Check the status of a pending charge.
+  ///
+  /// When you get pending as a charge status or if there was
+  /// an exception when calling any of the following methods [create],
+  /// [submitPin],[submitOtp],[submitPhone],[submitBirthday],[submitAddress],
+  /// wait 10 seconds or more, then make a check to see if
+  /// its status has changed.
+  /// Don't call too early as you may get a lot more pending than you should.
   Future<Response> pendingCharge(String reference) async {
     return await call(Uri.https(baseUrl, '/charge/$reference'), HttpMethod.get);
   }

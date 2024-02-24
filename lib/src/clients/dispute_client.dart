f4907b4d43386b0bc7e9e441863b193c73f3484e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-02-24 19:11:24 +0100

    Add documentation for DisputeClient

--- lib/src/clients/dispute_client.dart
+++ lib/src/clients/dispute_client.dart
@@ -1,9 +1,33 @@
 import '../base_client.dart';
 import '../enums.dart';
 
+/// It provides methods that mirror endpoints provided by
+/// Paystack's Disputes API which allows you manage transaction
+/// disputes on your integration.
 class DisputeClient extends BaseClient {
+  /// Create an instance of [DisputeClient].
+  ///
+  /// [secretKey] is your Paystack integration key. If omitted,
+  /// [DisputeClient] tries to load it from your environmental
+  /// variables with a key stored in its [secretKeyEnvironmentalVariableName]
+  /// which has a default of "PAYSTACK_SECRET_KEY". i.e. If you provide
+  /// your Paystack integration secret key in your environmental variables
+  /// as `PAYSTACK_SECRET_KEY=<your secret key>`, you don't need to provide
+  /// a [secretKey] on instantiation of [CustomerClient].
+  /// There is no need to instantiate this class directly unless you only
+  /// need to use features specific to this client as it is available in
+  /// the all encompassing [PaystackClient] via `disputes` binding
+  /// ## Example
+  /// ```dart
+  /// var client = DisputeClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.all();
+  /// // The Following code is an equivalent of the code above.
+  /// var client = PaystackClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.disputes.all();
+  /// ```
   DisputeClient({super.secretKey});
 
+  /// Retrieve all the disputes filed against your integration.
   Future<Response> all(
     String from,
     String to, {
@@ -24,15 +48,20 @@ class DisputeClient extends BaseClient {
         Uri.https(baseUrl, '/dispute', queryParameters), HttpMethod.get);
   }
 
+  /// Retrieve a single dispute by it's id
   Future<Response> fetchOne(String id) async {
     return await call(Uri.https(baseUrl, '/dispute/$id'), HttpMethod.get);
   }
 
+  /// Retrieve the dispute for a transaction.
+  ///
+  /// - [id] is id of transaction whose dispute you want to retrieve.
   Future<Response> transactionDisputes(String id) async {
     return await call(
         Uri.https(baseUrl, '/dispute/transaction/$id'), HttpMethod.get);
   }
 
+  /// Update the details of a dispute.
   Future<Response> update(String id, int refundAmount,
       {String? uploadedFilename}) async {
     return await call(Uri.https(baseUrl, '/dispute/$id'), HttpMethod.put,
@@ -42,6 +71,7 @@ class DisputeClient extends BaseClient {
         });
   }
 
+  /// Provide evidence for a dispute.
   Future<Response> addEvidence(String id, String customerEmail,
       String customerName, String customerPhone, String serviceDetails,
       {String? deliveryAddress, String? deliveryDate}) async {
@@ -58,6 +88,7 @@ class DisputeClient extends BaseClient {
         data: data);
   }
 
+  /// Retrieve a url to upload additional evidence.
   Future<Response> uploadUrl(String id, String uploadFilename) async {
     return await call(
         Uri.https(baseUrl, '/dispute/$id/upload_url',
@@ -65,6 +96,7 @@ class DisputeClient extends BaseClient {
         HttpMethod.get);
   }
 
+  /// Resolve a dispute on your integration.
   Future<Response> resolve(String id, DisputeResolution resolution,
       String message, int refundAmount, String uploadedFilename,
       {String? evidence}) async {
@@ -80,6 +112,7 @@ class DisputeClient extends BaseClient {
         data: data);
   }
 
+  /// Export disputes available on your integration.
   Future<Response> export(String from, String to,
       {int perPage = 50,
       int page = 1,

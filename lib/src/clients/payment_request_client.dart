708d729ee49c7ea82c0d884f2451c251889b695d
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-02-25 05:52:37 +0100

    Add documentation for PaymentRequestClient

--- lib/src/clients/payment_request_client.dart
+++ lib/src/clients/payment_request_client.dart
@@ -2,9 +2,33 @@ import '../base_client.dart';
 import '../enums.dart';
 import '../models.dart';
 
+/// It provides methods that mirror endpoints provided by
+/// Paystack's Payment Requests API which allows you manage requests
+/// for payment of goods and services.
 class PaymentRequestClient extends BaseClient {
+  /// Create an instance of [PaymentRequestClient].
+  ///
+  /// [secretKey] is your Paystack integration key. If omitted,
+  /// [PaymentRequestClient] tries to load it from your environmental
+  /// variables with a key stored in its [secretKeyEnvironmentalVariableName]
+  /// which has a default of "PAYSTACK_SECRET_KEY". i.e. If you provide
+  /// your Paystack integration secret key in your environmental variables
+  /// as `PAYSTACK_SECRET_KEY=<your secret key>`, you don't need to provide
+  /// a [secretKey] on instantiation of [PaymentRequestClient].
+  /// There is no need to instantiate this class directly unless you only
+  /// need to use features specific to this client as it is available in
+  /// the all encompassing [PaystackClient] via `paymentRequests` binding
+  /// ## Example
+  /// ```dart
+  /// var client = PaymentRequestClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.all();
+  /// // The Following code is an equivalent of the code above.
+  /// var client = PaystackClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.paymentRequests.all();
+  /// ```
   PaymentRequestClient({super.secretKey});
 
+  /// Create a payment request for a transaction on your integration
   Future<Response> create(String customer, int amount,
       {String? dueDate,
       String? description,
@@ -34,6 +58,7 @@ class PaymentRequestClient extends BaseClient {
         data: data);
   }
 
+  /// Retrieve the payment requests available on your integration
   Future<Response> all(
       {int perPage = 50,
       int page = 1,
@@ -57,32 +82,38 @@ class PaymentRequestClient extends BaseClient {
         Uri.https(baseUrl, '/paymentrequest', queryParameters), HttpMethod.get);
   }
 
+  /// Retrieve the details of a payment request on your integration
   Future<Response> fetchOne(String idOrCode) async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/$idOrCode'), HttpMethod.get);
   }
 
+  /// Verify details of a payment request on your integration
   Future<Response> verify(String code) async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/verify/$code'), HttpMethod.get);
   }
 
+  /// Send notification of a payment request to your customers
   Future<Response> sendNotification(String code) async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/notify/$code'), HttpMethod.post);
   }
 
+  /// Retrieve payment requests metric
   Future<Response> total() async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/totals'), HttpMethod.get);
   }
 
+  /// Finalize a draft payment request
   Future<Response> finalize(String code, bool sendNotification) async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/finalize/$code'), HttpMethod.post,
         data: {'send_notification': sendNotification});
   }
 
+  /// Update the details of a payment request on your integration
   Future<Response> update(
     String idOrCode,
     String customer,
@@ -115,6 +146,10 @@ class PaymentRequestClient extends BaseClient {
         data: data);
   }
 
+  /// Archive a payment request
+  ///
+  /// An archived payment request will no longer be retrieved on
+  /// call to [all] or [verify]
   Future<Response> archive(String code) async {
     return await call(
         Uri.https(baseUrl, '/paymentrequest/archive/$code'), HttpMethod.post);

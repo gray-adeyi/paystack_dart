6e98fb25a35abe66bc05d32b6690738c4b2397b0
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-02-25 07:46:28 +0100

    Add documentation for RecipientClient

--- lib/src/clients/verification_client.dart
+++ lib/src/clients/verification_client.dart
@@ -1,9 +1,33 @@
 import '../base_client.dart';
 import '../enums.dart';
 
+/// It provides methods that mirror endpoints provided by
+/// Paystack's Verification API which allows you perform KYC processes.
 class VerificationClient extends BaseClient {
+  /// Create an instance of [VerificationClient].
+  ///
+  /// [secretKey] is your Paystack integration key. If omitted,
+  /// [VerificationClient] tries to load it from your environmental
+  /// variables with a key stored in its [secretKeyEnvironmentalVariableName]
+  /// which has a default of "PAYSTACK_SECRET_KEY". i.e. If you provide
+  /// your Paystack integration secret key in your environmental variables
+  /// as `PAYSTACK_SECRET_KEY=<your secret key>`, you don't need to provide
+  /// a [secretKey] on instantiation of [VerificationClient].
+  /// There is no need to instantiate this class directly unless you only
+  /// need to use features specific to this client as it is available in
+  /// the all encompassing [PaystackClient] via `verification` binding
+  /// ## Example
+  /// ```dart
+  /// var client = VerificationClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.resolveAccount('3423452323', '801');
+  /// // The Following code is an equivalent of the code above.
+  /// var client = PaystackClient(secretKey: "<your paystack secret key>");
+  /// var response = await client.verification.resolveAccount(
+  /// '3423452323', '801');
+  /// ```
   VerificationClient({super.secretKey});
 
+  /// Confirm an account belongs to the right customer
   Future<Response> resolveAccount(
     String accountNumber,
     String bankCode,
@@ -14,6 +38,8 @@ class VerificationClient extends BaseClient {
         HttpMethod.get);
   }
 
+  /// Confirm the authenticity of a customer's account number
+  /// before sending money
   Future<Response> validateAccount(
       String accountName,
       String accountNumber,
@@ -35,6 +61,7 @@ class VerificationClient extends BaseClient {
         data: data);
   }
 
+  /// Retrieve more information about a customer's card
   Future<Response> resolveCardBin(String bin) async {
     return await call(Uri.https(baseUrl, '/decision/bin/$bin'), HttpMethod.get);
   }
